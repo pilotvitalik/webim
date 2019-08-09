5760e09cc3a613c1394bc17b86093fa582e15aa4
pilotvitalik <itvkip@yandex.ru>
2019-08-09 06:05:57 +0300

    ебольшие изменения

--- js/dynamic.js
+++ js/dynamic.js
@@ -2,7 +2,8 @@ let gamb = document.querySelector('.gamb');
 let reg = document.querySelector('.reg');
 let menu = document.querySelector('.menu');
 
-gumb.addEventListener('touchend',function(){
+
+gmb.addEventListener('touchend',function(){
 	reg.style.display = 'flex';
 	menu.style.display = 'flex';
-});
+})
